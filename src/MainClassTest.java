079363e067c24b25d3405749d2b8a3b91a43b473
asasimov <37741229+asasimov@users.noreply.github.com>
2018-10-07 14:28:01 +0300

    Добавлен тест testGetClassNumber

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,9 +3,15 @@ import org.junit.Test;
 
 public class MainClassTest {
 
+    MainClass mainClass = new MainClass();
+
     @Test
     public void testGetLocalNumber(){
-        MainClass mainClass = new MainClass();
         Assert.assertEquals("Values are not equals", mainClass.getLocalNumber(), 14);
+    }
+
+    @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("Method returned value <= 45", mainClass.getLocalNumber() > 45);
     }
 }
